bf14bce8575bdb753fc40214728d164ddfdb9d5d
AyaNS2003 <aya.sawalha.2003@gmail.com>
2024-09-12 23:50:25 +0300

    style(login) : adding styles to login page

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,47 +3,53 @@ import React from 'react'
 export default function index() {
   return (
     <>
-    <h1 className="display-1 text-center m-4">Login</h1>
-      <div className="container">
-        <form className="row g-3 needs-validation" novalidate>
-          <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" className="form-text">
-              We'll never share your email with anyone else.
+    
+      <div className="container login">
+        <div className="login-left">
+        <p className="display-1 text-center m-4">Login</p>
+          <form className="row g-3 needs-validation" novalidate>
+            <div className="mb-3">
+              <label for="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailHelp" className="form-text">
+                We'll never share your email with anyone else.
+              </div>
             </div>
-          </div>
-          <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <div className="mb-3 form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-            />
-            <label className="form-check-label" for="exampleCheck1">
-              Remember me
-            </label>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+            <div className="mb-3">
+              <label for="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+              />
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" for="exampleCheck1">
+                Remember me
+              </label>
+            </div>
+            <button type="submit" className=" submit-btn">
+              Submit
+            </button>
+          </form>
+        </div>
+        <div className="login-right">
+          <img src='./assets/Login.gif'/>
+        </div>
       </div>
     </>
   )
